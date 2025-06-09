846a69454104920cf53692b7b1fad9ef1d10df7e
yushulx <lingxiao1002@gmail.com>
2025-06-09 16:39:21 +0800

    Update barcode_ocr_text

--- examples/barcode_ocr_text/main.js
+++ examples/barcode_ocr_text/main.js
@@ -12,7 +12,7 @@ activateButton.onclick = async () => {
     try {
         Dynamsoft.License.LicenseManager.initLicense(license);
         Dynamsoft.Core.CoreModule.loadWasm(["cvr", "dbr", "dlr",]);
-        await Dynamsoft.DLR.LabelRecognizerModule.loadRecognitionData("Letter");
+        await Dynamsoft.CVR.CaptureVisionRouter.appendModelBuffer("LetterCharRecognition");
         let router = await Dynamsoft.CVR.CaptureVisionRouter.createInstance();
 
         let view = await Dynamsoft.DCE.CameraView.createInstance();
@@ -24,8 +24,6 @@ activateButton.onclick = async () => {
         const resultsContainer = document.querySelector("#results");
         const resultReceiver = new Dynamsoft.CVR.CapturedResultReceiver();
 
-        let lastResult = {};
-
         resultReceiver.onCapturedResultReceived = (result) => {
             if (result.items.length == 0) {
                 resultsContainer.textContent = '';
@@ -33,15 +31,6 @@ activateButton.onclick = async () => {
         }
 
         resultReceiver.onDecodedBarcodesReceived = (result) => {
-            if (!lastResult["id"]) {
-                lastResult["id"] = result.originalImageTag.imageId;
-            }
-            else {
-                if (lastResult["id"] !== result.originalImageTag.imageId) {
-                    resultsContainer.textContent = '';
-                    lastResult["id"] = result.originalImageTag.imageId;
-                }
-            }
 
             if (result.barcodeResultItems.length > 0) {
                 for (let item of result.barcodeResultItems) {
@@ -51,15 +40,6 @@ activateButton.onclick = async () => {
         }
 
         resultReceiver.onRecognizedTextLinesReceived = (result) => {
-            if (!lastResult["id"]) {
-                lastResult["id"] = result.originalImageTag.imageId;
-            }
-            else {
-                if (lastResult["id"] !== result.originalImageTag.imageId) {
-                    resultsContainer.textContent = '';
-                    lastResult["id"] = result.originalImageTag.imageId;
-                }
-            }
 
             if (result.textLineResultItems.length > 0) {
                 for (let item of result.textLineResultItems) {
@@ -73,110 +53,7 @@ activateButton.onclick = async () => {
         filter.enableResultCrossVerification(Dynamsoft.Core.EnumCapturedResultItemType.CRIT_BARCODE | Dynamsoft.Core.EnumCapturedResultItemType.CRIT_TEXT_LINE, true);
         filter.enableResultDeduplication(Dynamsoft.Core.EnumCapturedResultItemType.CRIT_BARCODE | Dynamsoft.Core.EnumCapturedResultItemType.CRIT_TEXT_LINE, true);
         await router.addResultFilter(filter);
-
-        let settings = {
-            "CaptureVisionTemplates": [
-                {
-                    "Name": "ReadBarcode&AccompanyText",
-                    "ImageROIProcessingNameArray": [
-                        "roi-read-barcodes-only", "roi-read-text"
-                    ]
-                }
-            ],
-            "TargetROIDefOptions": [
-                {
-                    "Name": "roi-read-barcodes-only",
-                    "TaskSettingNameArray": ["task-read-barcodes"]
-                },
-                {
-                    "Name": "roi-read-text",
-                    "TaskSettingNameArray": ["task-read-text"],
-                    "Location":
-                    {
-                        "ReferenceObjectFilter": {
-                            "ReferenceTargetROIDefNameArray": ["roi-read-barcodes-only"]
-                        },
-                        "Offset": {
-                            "MeasuredByPercentage": 1,
-                            "FirstPoint": [-20, -50],
-                            "SecondPoint": [150, -50],
-                            "ThirdPoint": [150, -5],
-                            "FourthPoint": [-20, -5]
-                        }
-                    }
-                }
-            ],
-            "CharacterModelOptions": [
-                {
-                    "Name": "Letter"
-                }
-            ],
-            "ImageParameterOptions": [
-                {
-                    "Name": "ip-read-text",
-                    "TextureDetectionModes": [
-                        {
-                            "Mode": "TDM_GENERAL_WIDTH_CONCENTRATION",
-                            "Sensitivity": 8
-                        }
-                    ],
-                    "TextDetectionMode": {
-                        "Mode": "TTDM_LINE",
-                        "CharHeightRange": [
-                            20,
-                            1000,
-                            1
-                        ],
-                        "Direction": "HORIZONTAL",
-                        "Sensitivity": 7
-                    }
-                }
-            ],
-            "TextLineSpecificationOptions": [
-                {
-                    "Name": "tls-11007",
-                    "CharacterModelName": "Letter",
-                    "CharHeightRange": [5, 1000, 1],
-                    "BinarizationModes": [
-                        {
-                            "BlockSizeX": 30,
-                            "BlockSizeY": 30,
-                            "Mode": "BM_LOCAL_BLOCK",
-                            "MorphOperation": "Close"
-                        }
-                    ]
-                }
-            ],
-            "BarcodeReaderTaskSettingOptions": [
-                {
-                    "Name": "task-read-barcodes",
-                    "BarcodeFormatIds": ["BF_ONED"]
-                }
-            ],
-            "LabelRecognizerTaskSettingOptions": [
-                {
-                    "Name": "task-read-text",
-                    "TextLineSpecificationNameArray": [
-                        "tls-11007"
-                    ],
-                    "SectionImageParameterArray": [
-                        {
-                            "Section": "ST_REGION_PREDETECTION",
-                            "ImageParameterName": "ip-read-text"
-                        },
-                        {
-                            "Section": "ST_TEXT_LINE_LOCALIZATION",
-                            "ImageParameterName": "ip-read-text"
-                        },
-                        {
-                            "Section": "ST_TEXT_LINE_RECOGNITION",
-                            "ImageParameterName": "ip-read-text"
-                        }
-                    ]
-                }
-            ]
-        }
-        await router.initSettings(settings);
+        await router.initSettings("./template.json");
 
         await cameraEnhancer.open();
         await router.startCapturing("ReadBarcode&AccompanyText");
